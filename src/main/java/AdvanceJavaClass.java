d8796b9a21f2617a528867b527fc8cb85ac9535d
sumathy vijay <sumathy.vijay@gmail.com>
2021-08-15 14:54:05 -0500

    Changes in AdvanceJavaClass

--- src/main/java/AdvanceJavaClass.java
+++ src/main/java/AdvanceJavaClass.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class AdvanceJavaClass {
+import org.apache.log4j.Logger;
+
+public class AdvanceJavaClass {
+
+
+    public static void main(String[] args) {
+
+        Logger log = Logger.getLogger(AdvanceJavaClass.class);
+
+        org.apache.log4j.BasicConfigurator.configure();
+        log.info("Java Class for Advanced Users");
+
+    }
 }
