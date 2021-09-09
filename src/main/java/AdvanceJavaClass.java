5f42226d799448f69c6b57773693f674e45b7204
sumathy vijay <sumathy.vijay@gmail.com>
2021-09-09 09:00:00 -0500

    Committing Changes to NewJavaProj

--- src/main/java/AdvanceJavaClass.java
+++ src/main/java/AdvanceJavaClass.java
@@ -1,14 +1,32 @@
+package com.sample;
+
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 public class AdvanceJavaClass {
 
-
-    public static void main(String[] args) {
+    public static void main(String args[]) {
 
         Logger log = Logger.getLogger(AdvanceJavaClass.class);
 
-        org.apache.log4j.BasicConfigurator.configure();
-        log.info("Java Class for Advanced Users");
+        BasicConfigurator.configure();
+
+        log.info("Java class for Advanced users");
+
+        log.info("Argument :" + args[0]);
+        log.info("Argument :" + args[1]);
+        log.info("Argument :" + args[2]);
+        log.info("Argument :" + args[3]);
+        log.info("Argument :" + args[4]);
+        log.info("Argument :" + args[5]);
+        log.info("Argument :" + args[6]);
+
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = a + b;
+        log.info("Add a and b =" + c);
 
     }
+
 }
